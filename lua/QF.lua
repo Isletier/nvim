d6367a6893a7026c9c7eb0f0cb010efbc6d7a119
Lufflee-Vaflee <garriharripuff@gmail.com>
2026-06-25 12:14:31 +0300

    qf attempts

--- lua/QF.lua
+++ lua/QF.lua
@@ -82,6 +82,55 @@ function BuffersToQf()
     Open_qf_standart()
 end
 
+local track_file = "/tmp/qf_stream.txt"
+local last_size = 0
+local is_watching = false -- Your toggle flag
+
+local function watch_step()
+    if not is_watching then return end
+
+    local f = io.open(track_file, "r")
+    if not f then return end
+
+    local current_size = f:seek("end")
+
+    -- If file shrank or reset, clear the quickfix list and start over
+    if current_size < last_size then
+        vim.cmd("cclear")
+        last_size = 0
+    end
+
+    -- If new content was added, read only the new portion
+    if current_size > last_size then
+        f:seek("set", last_size)
+        local new_content = f:read("*a")
+        last_size = current_size
+
+        -- Convert the new string block into a list of lines
+        local lines = vim.split(new_content, "\n")
+
+        -- 'a' means APPEND to the existing quickfix list instead of overwriting
+        vim.fn.setqflist({}, 'a', { lines = lines })
+    end
+
+    f:close()
+end
+
+-- Toggle function you can bind to a key
+function Toggle_pinned_qf()
+    is_watching = not is_watching
+    if is_watching then
+        print("Quickfix watching enabled")
+        last_size = 0
+        _G.watch_timer = vim.loop.new_timer()
+        _G.watch_timer:start(0, 400, vim.schedule_wrap(watch_step))
+    else
+        print("Quickfix watching disabled")
+        if _G.watch_timer then _G.watch_timer:close() end
+    end
+end
+
+
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
@@ -89,6 +138,7 @@ vim.keymap.set("n", "<leader>qg", Open_qf_full)
 vim.keymap.set("n", "<leader>qn", Open_qf_standart)
 vim.keymap.set("n", "<leader>qe", "<cmd>cclose<CR>")
 vim.keymap.set("n", "<leader>qf", toggle_qf_filter)
+vim.keymap.set('n', '<leader>qt', Toggle_pinned_qf, { desc = "Load streamed terminal output into Quickfix" })
 
 vim.keymap.set("n", "<leader>ql", vim.diagnostic.setqflist, { desc = "LSP diagnostics to quickfix" })
 vim.keymap.set("n", "<leader>qb", BuffersToQf, { desc = "Buffers to quickfix" })
